54c3043f22f4dad3dafb11d59b9c18e5fa8200fd
Axel Nana <axel.nana@aliens-group.com>
2021-10-23 18:32:00 +0100

    feat(rendering): :sparkles: Implement a base renderer class
    Will be implemented by platform specific code to provide customized rendering features.

---
+++ include/SparkyStudios/UI/Pixel/Core/Renderer/BaseRenderer.h
@@ -0,0 +1,367 @@
+// Copyright (c) 2021-present Sparky Studios. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#ifndef PIXEL_UI_BASERENDERER_H
+#define PIXEL_UI_BASERENDERER_H
+
+#include <SparkyStudios/UI/Pixel/Core/Common.h>
+#include <SparkyStudios/UI/Pixel/Core/Resource.h>
+
+#include <SparkyStudios/UI/Pixel/Graphics/Color.h>
+#include <SparkyStudios/UI/Pixel/Graphics/Font.h>
+#include <SparkyStudios/UI/Pixel/Graphics/Point.h>
+#include <SparkyStudios/UI/Pixel/Graphics/Rect.h>
+#include <SparkyStudios/UI/Pixel/Graphics/Texture.h>
+
+namespace SparkyStudios::UI::Pixel
+{
+    class MainWindow;
+
+    class BaseRenderer;
+
+    class ICacheToTexture
+    {
+    public:
+        typedef void* CacheHandle;
+
+        virtual ~ICacheToTexture() = default;
+
+        virtual void Initialize() = 0;
+        virtual void ShutDown() = 0;
+        virtual void SetupCacheTexture(CacheHandle control) = 0;
+        virtual void FinishCacheTexture(CacheHandle control) = 0;
+        virtual void DrawCachedControlTexture(CacheHandle control) = 0;
+        virtual void CreateControlCacheTexture(CacheHandle control, const Point& size) = 0;
+        virtual void UpdateControlCacheTexture(CacheHandle control) = 0;
+        virtual void SetRenderer(BaseRenderer* renderer) = 0;
+    };
+
+    /**
+     * @brief Base class for platform specific renderer implementations.
+     */
+    class PI_EXPORT BaseRenderer : public IResourceLoader
+    {
+    protected:
+        /**
+         * @brief Constructor
+         *
+         * @param paths The resources path manager to use in this
+         * renderer.
+         */
+        BaseRenderer(ResourcePaths& paths);
+
+    public:
+        virtual ~BaseRenderer();
+
+        /**
+         * @brief Initializes the renderer.
+         */
+        virtual void Init();
+
+        /**
+         * @brief Begins the rendering process.
+         *
+         * Every needed resource should be loaded in this method.
+         */
+        virtual void Begin();
+
+        /**
+         * @brief Ends the rendering process.
+         *
+         * Every loaded resource should be released in this method.
+         */
+        virtual void End();
+
+        /**
+         * @brief Gets the IResourceLoader object.
+         *
+         * @return The resource loader instance associated with this renderer.
+         */
+        IResourceLoader& GetLoader();
+
+        /**
+         * @brief Gets the Resource Paths object
+         *
+         * @return ResourcePaths&
+         */
+        const ResourcePaths& GetResourcePaths() const;
+
+        /**
+         * @brief Sets the color of next draw operations.
+         *
+         * @param color The color of next draw operations.
+         */
+        virtual void SetDrawColor(const Color& color);
+
+        /**
+         * @brief Clips the renderer inside the region defined
+         * by BaseRenderer::SetClipRegion().
+         */
+        virtual void StartClip();
+
+        /**
+         * @brief Restores the clip region of the renderer.
+         */
+        virtual void EndClip();
+
+        /**
+         * @brief Set the clip region rectangle to use in BaseRenderer::StartClip().
+         *
+         * @param rect The clip region.
+         */
+        void SetClipRegion(Rect const& rect);
+
+        /**
+         * @brief Add a clip region to the current one.
+         *
+         * @param rect The clip region to add.
+         */
+        void AddClipRegion(Rect rect);
+
+        /**
+         * @brief Check if the clip region is valid and visible.
+         *
+         * @return Whether the clip region is visible.
+         */
+        bool ClipRegionVisible();
+
+        /**
+         * @brief Gets the clip region.
+         *
+         * @return The clip region.
+         */
+        const Rect& ClipRegion() const;
+
+        /**
+         * @brief Gets the color of the pixel at the given position.
+         *
+         * @param texture The texture to fetch the pixel from.
+         * @param position The position of the pixel.
+         * @param defaultColor The default color to use as fallback.
+         *
+         * @return The pixel color.
+         */
+        virtual Color PixelColor(const Texture& texture, const Point& position, const Color& defaultColor = Colors::White);
+
+        /**
+         * @brief Fallback drawer used when trying to draw an unloaded or missing image.
+         *
+         * @param rect The target space to draw into.
+         */
+        virtual void DrawMissingImage(Rect rect);
+
+        /**
+         * @brief Draws a rectangle filled with the drawing color.
+         *
+         * @param rect The target space to draw into.
+         */
+        virtual void DrawFilledRect(Rect rect);
+
+        /**
+         * @brief Draws a texture.
+         *
+         * @param texture The texture to draw.
+         * @param rect The target space to draw into.
+         * @param u1 The X-axis upper left coordinate inside the texture.
+         * @param v1 The Y-axis upper left coordinate inside the texture.
+         * @param u2 The X-axis lower right coordinate inside the texture.
+         * @param v2 The Y-axis lower right coordinate inside the texture.
+         */
+        virtual void DrawTexturedRect(
+            const Texture& texture, Rect rect, PiReal32 u1 = 0.0f, PiReal32 v1 = 0.0f, PiReal32 u2 = 1.0f, PiReal32 v2 = 1.0f);
+
+        /**
+         * @brief Draws a transparent rectangle with borders of width 1.
+         *
+         * @param rect The target space to draw into.
+         */
+        virtual void DrawLinedRect(Rect rect);
+
+        /**
+         * @brief Draws a single pixel with the drawing color.
+         *
+         * @param position The pixel position.
+         */
+        virtual void DrawPixel(const Point& position);
+
+        /**
+         * @brief Draws a rectangle with shaved corners.
+         *
+         * @param rect The target space to draw into.
+         * @param bSlight
+         */
+        virtual void DrawShavedCornerRect(Rect rect, bool bSlight = false);
+
+        /**
+         * @brief Draws a text with the given font at the given position.
+         *
+         * @param font The font to use when drawing the text.
+         * @param pos The position of the text.
+         * @param text The text to draw.
+         */
+        virtual void DrawText(const Font& font, Point pos, const PiString& text);
+
+        /**
+         * @brief Gets the size of the text.
+         *
+         * @param font The font to use when computing the size.
+         * @param text The text.
+         *
+         * @return The size of the text in pixels.
+         */
+        virtual Size MeasureText(const Font& font, const PiString& text);
+
+        /**
+         * @brief Converts a position in pixels to render space coordinates.
+         *
+         * @param x The x coordinate.
+         * @param y The y coordinate.
+         */
+        void Translate(int& x, int& y);
+
+        /**
+         * @brief Converts a rectangle in pixels to render space coordinates.
+         *
+         * @param rect The rectangle.
+         */
+        void Translate(Rect& rect);
+
+        /**
+         * @brief Sets the render offset.
+         *
+         * @param offset The offset in pixels.
+         */
+        void SetRenderOffset(const Point& offset);
+
+        /**
+         * @brief Adds the given position to the render offset.
+         *
+         * @param offset The offset to add.
+         */
+        void AddRenderOffset(const Rect& offset);
+
+        /**
+         * @brief Gets the current render offset.
+         *
+         * @return The current render offset.
+         */
+        const Point& GetRenderOffset() const;
+
+        /**
+         * @brief Sets the UI scale factor.
+         *
+         * @param scale The scale factor.
+         */
+        void SetScale(PiReal32 scale);
+
+        /**
+         * @brief Gets the UI scale factor.
+         *
+         * @return The scale factor.
+         */
+        PiReal32 GetScale() const;
+
+        /**
+         * @brief Initializes the rendering context.
+         *
+         * @param window The window which hold the rendering context.
+         *
+         * @return Whether the operation was successful.
+         */
+        virtual bool InitializeContext(MainWindow* window);
+
+        /**
+         * @brief Destroys the rendering context.
+         *
+         * @param window The window which holds the rendering context.
+         *
+         * @return Whether the operation was successful.
+         */
+        virtual bool DestroyContext(MainWindow* window);
+
+        /**
+         * @brief Resizes the rendering context.
+         *
+         * @param window The window which holds the rendering context.
+         * @param size The new size of the rendering context.
+         *
+         * @return Whether the operation was successful.
+         */
+        virtual bool ResizedContext(MainWindow* window, const Size& size);
+
+        /**
+         * @brief Starts a rendering session.
+         *
+         * @param window The window which holds the rendering context.
+         *
+         * @return Whether the operation was successful.
+         */
+        virtual bool BeginContext(MainWindow* window);
+
+        /**
+         * @brief Ends a rendering session.
+         *
+         * @param window The window which holds the rendering context.
+         *
+         * @return Whether the operation was successful.
+         */
+        virtual bool EndContext(MainWindow* window);
+
+        /**
+         * @brief Swap front and back buffers in the rendering context.
+         *
+         * @param window The window which holds the rendering context.
+         *
+         * @return Whether the operation was successful.
+         */
+        virtual bool PresentContext(MainWindow* window);
+
+        /**
+         * @brief Gets the widgets texture cache.
+         *
+         * @return ICacheToTexture*
+         */
+        virtual ICacheToTexture* GetCTT();
+
+    protected:
+        /**
+         * @brief Ensures that a font resource is loaded and available.
+         *
+         * @param font The font resource.
+         *
+         * @return true when the font resource is loaded, false otherwise.
+         */
+        virtual bool EnsureFont(const Font& font);
+
+        /**
+         * @brief Ensures that a texture resource is loaded and available.
+         *
+         * @param texture The texture resource.
+         *
+         * @return true when the texture resource is loaded, false otherwise.
+         */
+        virtual bool EnsureTexture(const Texture& texture);
+
+        PiReal32 m_scale;
+
+    private:
+        ResourcePaths& _paths;
+        Point _renderOffset;
+        Rect _rectClipRegion;
+    };
+} // namespace SparkyStudios::UI::Pixel
+
+#endif // PIXEL_UI_BASERENDERER_H
---
+++ src/Core/Renderer/BaseRenderer.cpp
@@ -0,0 +1,301 @@
+// Copyright (c) 2021-present Sparky Studios. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cmath>
+
+#include <SparkyStudios/UI/Pixel/Core/Renderer/BaseRenderer.h>
+#include <SparkyStudios/UI/Pixel/Core/Utility.h>
+
+namespace SparkyStudios::UI::Pixel
+{
+    BaseRenderer::BaseRenderer(ResourcePaths& paths)
+        : m_scale(1.0f)
+        , _paths(paths)
+        , _renderOffset(Point(0, 0))
+    {}
+
+    BaseRenderer::~BaseRenderer()
+    {
+        if (GetCTT())
+            GetCTT()->ShutDown();
+    }
+
+    void BaseRenderer::Init()
+    {}
+
+    void BaseRenderer::Begin()
+    {}
+
+    void BaseRenderer::End()
+    {}
+
+    IResourceLoader& BaseRenderer::GetLoader()
+    {
+        return *reinterpret_cast<IResourceLoader*>(this);
+    }
+
+    const ResourcePaths& BaseRenderer::GetResourcePaths() const
+    {
+        return _paths;
+    }
+
+    void BaseRenderer::SetDrawColor(const Color& color)
+    {}
+
+    void BaseRenderer::StartClip()
+    {}
+
+    void BaseRenderer::EndClip()
+    {}
+
+    void BaseRenderer::SetClipRegion(const Rect& rect)
+    {
+        _rectClipRegion = rect;
+    }
+
+    void BaseRenderer::AddClipRegion(Rect rect)
+    {
+        rect.x += _renderOffset.x;
+        rect.y += _renderOffset.y;
+        Rect out = rect;
+
+        if (rect.x < _rectClipRegion.x)
+        {
+            out.w -= _rectClipRegion.x - out.x;
+            out.x = _rectClipRegion.x;
+        }
+
+        if (rect.y < _rectClipRegion.y)
+        {
+            out.h -= _rectClipRegion.y - out.y;
+            out.y = _rectClipRegion.y;
+        }
+
+        if (rect.x + rect.w > _rectClipRegion.x + _rectClipRegion.w)
+            out.w = (_rectClipRegion.x + _rectClipRegion.w) - out.x;
+
+        if (rect.y + rect.h > _rectClipRegion.y + _rectClipRegion.h)
+            out.h = (_rectClipRegion.y + _rectClipRegion.h) - out.y;
+
+        _rectClipRegion = out;
+    }
+
+    bool BaseRenderer::ClipRegionVisible()
+    {
+        if (_rectClipRegion.w <= 0 || _rectClipRegion.h <= 0)
+            return false;
+
+        return true;
+    }
+
+    const Rect& BaseRenderer::ClipRegion() const
+    {
+        return _rectClipRegion;
+    }
+
+    Color BaseRenderer::PixelColor(const Texture& texture, const Point& position, const Color& defaultColor)
+    {
+        return defaultColor;
+    }
+
+    void BaseRenderer::DrawMissingImage(Rect rect)
+    {
+        SetDrawColor(Colors::Red);
+        DrawFilledRect(rect);
+    }
+
+    void BaseRenderer::DrawFilledRect(Rect rect)
+    {}
+
+    void BaseRenderer::DrawTexturedRect(const Texture& texture, Rect rect, PiReal32 u1, PiReal32 v1, PiReal32 u2, PiReal32 v2)
+    {}
+
+    void BaseRenderer::DrawLinedRect(Rect rect)
+    {
+        DrawFilledRect(Rect(rect.x, rect.y, rect.w, 1));
+        DrawFilledRect(Rect(rect.x, rect.y + rect.h - 1, rect.w, 1));
+        DrawFilledRect(Rect(rect.x, rect.y, 1, rect.h));
+        DrawFilledRect(Rect(rect.x + rect.w - 1, rect.y, 1, rect.h));
+    }
+
+    void BaseRenderer::DrawPixel(const Point& position)
+    {
+        DrawFilledRect(Rect(position.x, position.y, 1, 1));
+    }
+
+    void BaseRenderer::DrawShavedCornerRect(Rect rect, bool bSlight)
+    {
+        // Draw INSIDE the w/h.
+        rect.w -= 1;
+        rect.h -= 1;
+
+        if (bSlight)
+        {
+            DrawFilledRect(Rect(rect.x + 1, rect.y, rect.w - 1, 1));
+            DrawFilledRect(Rect(rect.x + 1, rect.y + rect.h, rect.w - 1, 1));
+            DrawFilledRect(Rect(rect.x, rect.y + 1, 1, rect.h - 1));
+            DrawFilledRect(Rect(rect.x + rect.w, rect.y + 1, 1, rect.h - 1));
+        }
+        else
+        {
+            DrawPixel(Point(rect.x + 1, rect.y + 1));
+            DrawPixel(Point(rect.x + rect.w - 1, rect.y + 1));
+            DrawPixel(Point(rect.x + 1, rect.y + rect.h - 1));
+            DrawPixel(Point(rect.x + rect.w - 1, rect.y + rect.h - 1));
+            DrawFilledRect(Rect(rect.x + 2, rect.y, rect.w - 3, 1));
+            DrawFilledRect(Rect(rect.x + 2, rect.y + rect.h, rect.w - 3, 1));
+            DrawFilledRect(Rect(rect.x, rect.y + 2, 1, rect.h - 3));
+            DrawFilledRect(Rect(rect.x + rect.w, rect.y + 2, 1, rect.h - 3));
+        }
+    }
+
+    void BaseRenderer::DrawText(const Font& font, Point pos, const PiString& text)
+    {
+        const float fSize = font.size * GetScale();
+
+        for (unsigned int i = 0; i < text.length(); i++)
+        {
+            char chr = text[i];
+
+            if (chr == ' ')
+                continue;
+
+            Rect r(pos.x + i * fSize * 0.4f, pos.y, fSize * 0.4f - 1.0f, fSize);
+
+            // This isn't important, it's just me messing around changing
+            // the shape of the rect based on the letter.. just for fun.
+            if (chr == 'l' || chr == 'i' || chr == '!' || chr == 't')
+            {
+                r.w = 1;
+            }
+            else if (chr >= 'a' && chr <= 'z')
+            {
+                r.y += fSize * 0.5f;
+                r.h -= fSize * 0.4f;
+            }
+            else if (chr == '.' || chr == ',')
+            {
+                r.x += 2;
+                r.y += r.h - 2;
+                r.w = 2;
+                r.h = 2;
+            }
+            else if (chr == '\'' || chr == '`' || chr == '"')
+            {
+                r.x += 3;
+                r.w = 2;
+                r.h = 2;
+            }
+
+            if (chr == 'o' || chr == 'O' || chr == '0')
+                DrawLinedRect(r);
+            else
+                DrawFilledRect(r);
+        }
+    }
+
+    Size BaseRenderer::MeasureText(const Font& font, const PiString& text)
+    {
+        Size p;
+        p.x = font.size * GetScale() * text.length() * 0.4f;
+        p.y = font.size * GetScale();
+        return p;
+    }
+
+    void BaseRenderer::Translate(int& x, int& y)
+    {
+        x += _renderOffset.x;
+        y += _renderOffset.y;
+        x = std::ceil(float(x) * m_scale);
+        y = std::ceil(float(y) * m_scale);
+    }
+
+    void BaseRenderer::Translate(Rect& rect)
+    {
+        Translate(rect.x, rect.y);
+        rect.w = std::ceil(float(rect.w) * m_scale);
+        rect.h = std::ceil(float(rect.h) * m_scale);
+    }
+
+    void BaseRenderer::SetRenderOffset(const Point& offset)
+    {
+        _renderOffset = offset;
+    }
+
+    void BaseRenderer::AddRenderOffset(const Rect& offset)
+    {
+        _renderOffset.x += offset.x;
+        _renderOffset.y += offset.y;
+    }
+
+    const Point& BaseRenderer::GetRenderOffset() const
+    {
+        return _renderOffset;
+    }
+
+    void BaseRenderer::SetScale(PiReal32 scale)
+    {
+        m_scale = scale;
+    }
+
+    PiReal32 BaseRenderer::GetScale() const
+    {
+        return m_scale;
+    }
+
+    bool BaseRenderer::InitializeContext(MainWindow* window)
+    {
+        return false;
+    }
+
+    bool BaseRenderer::DestroyContext(MainWindow* window)
+    {
+        return false;
+    }
+
+    bool BaseRenderer::ResizedContext(MainWindow* window, const Size& size)
+    {
+        return false;
+    }
+
+    bool BaseRenderer::BeginContext(MainWindow* window)
+    {
+        return false;
+    }
+
+    bool BaseRenderer::EndContext(MainWindow* window)
+    {
+        return false;
+    }
+
+    bool BaseRenderer::PresentContext(MainWindow* window)
+    {
+        return false;
+    }
+
+    ICacheToTexture* BaseRenderer::GetCTT()
+    {
+        return nullptr;
+    }
+
+    bool BaseRenderer::EnsureFont(const Font& font)
+    {
+        return false;
+    }
+
+    bool BaseRenderer::EnsureTexture(const Texture& texture)
+    {
+        return false;
+    }
+} // namespace SparkyStudios::UI::Pixel
